7a8174c8f9ae600d66ed2a804736142c1f1833f8
Chibinl <74690915+Chibinl@users.noreply.github.com>
2021-01-18 17:07:32 +0800

    工业品爱消除

--- wl_jd_gyec.js
+++ wl_jd_gyec.js
@@ -7,7 +7,7 @@
 /*
 工业品爱消除
 活动共200关，通关可获得3星，600星可兑换1888京豆，按照cron运行只需7天即可得到
-感谢@yogayyy(https://github.com/yogayyy/Scripts)制作的图标
+感谢@Chipun制作的图标
 活动入口：京东app首页-京东工业品-京东工业品年末盛典-勇闯消消乐
 已支持IOS双京东账号,Node.js支持N个京东账号
 boxjs 填写具体兑换商品的名称，默认为1888京豆
@@ -15,17 +15,17 @@ boxjs 填写具体兑换商品的名称，默认为1888京豆
 ============Quantumultx===============
 [task_local]
 #工业品爱消除
-30 * * * * https://raw.githubusercontent.com/shylocks/Loon/main/jd_gyec.js, tag=工业品爱消除, img-url=https://raw.githubusercontent.com/yogayyy/Scripts/main/Icon/shylock/jd_gyxxl.png, enabled=true
+30 * * * * https://raw.githubusercontent.com/Chibinl/JD-JX/main/wl_jd_gyec.js, tag=工业品爱消除, img-url=https://raw.githubusercontent.com/yogayyy/Scripts/main/Icon/shylock/jd_gyxxl.png, enabled=true
 
 ================Loon==============
 [Script]
-cron "30 * * * *" script-path=https://raw.githubusercontent.com/shylocks/Loon/main/jd_gyec.js,tag=工业品爱消除
+cron "30 * * * *" script-path=https://raw.githubusercontent.com/Chibinl/JD-JX/main/wl_jd_gyec.js,tag=工业品爱消除
 
 ===============Surge=================
-工业品爱消除 = type=cron,cronexp="30 * * * *",wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/shylocks/Loon/main/jd_gyec.js
+工业品爱消除 = type=cron,cronexp="30 * * * *",wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/Chibinl/JD-JX/main/wl_jd_gyec.js
 
 ============小火箭=========
-工业品爱消除 = type=cron,script-path=https://raw.githubusercontent.com/shylocks/Loon/main/jd_gyec.js, cronexpr="30 * * * *", timeout=200, enable=true
+工业品爱消除 = type=cron,script-path=https://raw.githubusercontent.com/Chibinl/JD-JX/main/wl_jd_gyec.js, cronexpr="30 * * * *", timeout=200, enable=true
  */
 const $ = new Env('工业品爱消除');
 const notify = $.isNode() ? require('./sendNotify') : '';
