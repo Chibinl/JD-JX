1815dd7089be9b86f45125c4fcd68d62f8da1675
Chibinl <74690915+Chibinl@users.noreply.github.com>
2021-03-02 15:16:41 +0800

    京东多合一签到

--- JD_DailyBonus.js
+++ JD_DailyBonus.js
@@ -15,7 +15,7 @@
 
 开启抓包app后, Safari浏览器登录 https://bean.m.jd.com/bean/signIndex.action 点击签到并且出现签到日历后, 返回抓包app搜索关键字 functionId=signBean 复制请求头Cookie填入以下Key处的单引号内即可 */
 
-var Key = ''; //单引号内自行填写您抓取的Cookie
+var Key = 'pt_pin=zihuangtan19424;pt_key=AAJgPeXYADBzZYOdtFpXKHMGBN5wgdNqTluRCkDA_YS6KQTIwKxZN9rkHu7OGQZd6FvwH-ErPkI;'; //单引号内自行填写您抓取的Cookie
 
 var DualKey = ''; //如需双账号签到,此处单引号内填写抓取的"账号2"Cookie, 否则请勿填写
 
